62cee161cd54167ab0074554f73948bafd68f168
Joshua Webster <64966534+joshweb0310@users.noreply.github.com>
2026-05-05 17:46:19 -0400

    Implement disk usage functions in disk.sh
    Add functions to get and render root disk usage

--- modules/modules/modules/modules/disk.sh
+++ modules/modules/modules/modules/disk.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+
+get_root_disk_usage() {
+  # Returns: used_percent used total mount
+  df -h / | awk 'NR==2 {print $5, $3, $2, $6}'
+}
+
+render_disk_section() {
+  local percent used total mount
+  read -r percent used total mount < <(get_root_disk_usage)
+  printf " Disk  : %4s used  (%s / %s on %s)\n" "$percent" "$used" "$total" "$mount"
+}
 
